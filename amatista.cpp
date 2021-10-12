1e126491a3c12e93d7cc1df00d5011744ebcd7b8
Rodríguez Poggio, Darío Andrés <rodriguez.dario.a@gmail.com>
2021-10-11 21:16:28 -0300

    Funcionalidad de Funcion mejor delineada
    Falta definir la tabla de simbolos y hacer el lookup y ejecución
    
    Signed-off-by: Rodríguez Poggio, Darío Andrés <rodriguez.dario.a@gmail.com>

--- amatista.cpp
+++ amatista.cpp
@@ -77,24 +77,41 @@ namespace Amatista {
 	class Lista : public Objeto {
 	public:
 		Lista ()
-			{_n = 0;}
+			{_n = 0; _linea = 0; _columna = 0;}
 		Lista (unsigned int n)
-			{_n = n;}
+			{_n = n; _linea = 0; _columna = 0;}
+
 
 		auto vector ()
 			{return &_v;}
 		auto nivel ()
 			{return _n;}
+		auto linea ()
+			{return _linea;}
+		auto columna ()
+			{return _columna;}
+		auto tamaño ()
+			{return this->vector ()->size ();}
+		const Objeto& operator[] (std::size_t idx)
+			{ return *(this->vector ()->at (idx)); }
+
+
+		void linea (const unsigned int l)
+			{_linea = l;}
+		void columna (const unsigned int c)
+			{_columna = c;}
 
 		void agregar (const std::shared_ptr<Objeto>& o)
 			{this->vector()->push_back(o);}
+		void quitarPrimero (void)
+			{this->vector()->erase (this->vector()->begin());}
 
-		void mostrar (void)
+		void mostrarDetalle (void)
 			{
 				std::cout << "nivel: "<< this->nivel () << " elementos: " << this->vector()->size() << "\n";
-				for ( const auto &i : _v )
-					std::cout << *i;
-				std::cout << std::endl;
+				// for ( const auto &i : _v )
+				// 	std::cout << *i;
+				// std::cout << *this << std::endl;
 			}
 
 		std::ostream& mostrar (std::ostream &salida) const
@@ -110,16 +127,23 @@ namespace Amatista {
 		friend std::istream& operator>> (std::istream& entrada, Lista& b)
 			{
 				b.leerLista (entrada);
-				b.ejecutar ();
-				b.mostrar ();
+				b.ejecutar (b);
+				b.mostrarDetalle ();
 				return entrada;
+			}
+
+		Lista& ejecutar (Lista& l)
+			{
+				// std::cout << "llamada a Lista.ejecutar ("
+				//   << this->linea () << this->columna () << ")" << std::endl;
+				return l;
 			}
 
 	private:
 		std::vector<std::shared_ptr<Objeto>> _v;
 		unsigned int _n;
-
-		void ejecutar () {}
+		unsigned int _linea;
+		unsigned int _columna;
 
 		std::string leerTexto (std::istream& archivo)
 			{
@@ -128,8 +152,11 @@ namespace Amatista {
 				while (archivo >> std::noskipws >> c1
 					   && c1 != '"'
 					   && c1 != '\'')
+				{
+					_columna++;
 					r += c1;
-				std::cout << "DEBUG TEXTO ["<<r<<"]"<<std::endl;
+				}
+//				std::cout << "DEBUG TEXTO ["<<r<<"]"<<std::endl;
 				return r;
 			}
 
@@ -141,8 +168,11 @@ namespace Amatista {
 					   && c1 != ' '
 					   && c1 != '\t'
 					   && c1 != '\n')
+				{
+					_columna++;
 					r += c1;
-				std::cout << "DEBUG SIMBOLO ["<<r<<"]"<<std::endl;
+				}
+//				std::cout << "DEBUG SIMBOLO ["<<r<<"]"<<std::endl;
 				return r;
 			}
 
@@ -153,6 +183,7 @@ namespace Amatista {
 				while (archivo >> std::noskipws >> c1)
 				{
 					// std::cout << "DEBUG LEÍDO ["<<c1<<"]"<<std::endl;
+					_columna++;
 					if (c1 == '(')
 					{
 						auto l1 = std::make_shared<Lista>(this->nivel()+1);
@@ -172,12 +203,18 @@ namespace Amatista {
 					else if (c1 != '\n' && c1 != ' ' && c1 != '\t')
 					{
 						archivo.putback (c1);
+						_columna--;
 						auto s1 = leerSimbolo (archivo);
 						auto o = std::make_shared<Objeto>(s1);
 						this->agregar (o);
 					}
+					else if (c1 == '\n')
+					{
+						_columna = 0;
+						_linea++;
+					}
 				}
-				std::cout << "DEBUG FUERA DE BUCLE NIVEL " << this->nivel() << std::endl;
+//				std::cout << "DEBUG FUERA DE BUCLE NIVEL " << this->nivel() << std::endl;
 			}
 	};
 
@@ -187,12 +224,23 @@ namespace Amatista {
 			{this->_nombre = n; this->_f=f;}
 
 		Lista& ejecutar (Lista& params)
-			{return _f (params);}
+			{return _f (params.ejecutar(params));}
 	private:
 		std::function<Lista&(Lista&)> _f;
 	};
 
-	Funcion mostrador ("mostrar", [] (Lista& l) -> Lista& {l.mostrar(); return l;});
+	Funcion mostrar ("mostrar",
+					   [] (Lista& l) -> Lista& {
+						   l.quitarPrimero();
+						   if (l.tamaño () == 1) {
+							   // TODO este cout debe ser solo en caso que sea texto
+							   std::cout << ((Texto&)l[0]).contenido () << std::endl;
+						   }
+						   else {
+							   std::cout << l << std::endl;
+						   }
+						   return l;
+					   });
 
 }
 
@@ -202,5 +250,5 @@ int main (void)
 	std::ifstream archivo("test.amt", std::ios::binary);
 	archivo >> loop;
 	std::cout << "MOSTRANDO" << std::endl;
-	Amatista::mostrador.ejecutar (loop);
+	Amatista::mostrar.ejecutar ((Amatista::Lista&) loop [0]);
 }
